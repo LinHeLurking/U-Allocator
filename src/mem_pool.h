bc3388aae093a82bc71b9df22ae5e25f1b8a44d5
LinHeLurking <LinHe.Lurking@gmail.com>
2022-07-31 17:07:33 +0800

    Added multi-sized memory pool;

---
+++ src/mem_pool.h
@@ -0,0 +1,370 @@
+#ifndef UALLOCATOR_MEM_POOL_H
+#define UALLOCATOR_MEM_POOL_H
+
+#include <stddef.h>
+#include <stdlib.h>
+
+#include <algorithm>
+
+namespace UAllocator {
+namespace Detail {
+
+template <typename Int32,
+          typename std::enable_if<std::is_integral<Int32>::value &&
+                                      sizeof(Int32) == 4,
+                                  bool>::type = true>
+Int32 round2pow(Int32 x) {
+  return x == 0 ? 1 : ((x & (x - 1)) == 0) ? x : 1 << (32 - __builtin_clz(x));
+}
+
+template <typename Int64,
+          typename std::enable_if<std::is_integral<Int64>::value &&
+                                      sizeof(Int64) == 8,
+                                  bool>::type = true>
+Int64 round2pow(Int64 x) {
+  return x == 0                 ? 1
+         : ((x & (x - 1)) == 0) ? x
+                                : 1UL << (64 - __builtin_clzll(x));
+}
+
+template <typename Int64,
+          typename std::enable_if<std::is_integral<Int64>::value &&
+                                      sizeof(Int64) == 8,
+                                  bool>::type = true>
+Int64 ctz(Int64 x) {
+  return __builtin_ctzll(x);
+}
+
+template <typename Int32,
+          typename std::enable_if<std::is_integral<Int32>::value &&
+                                      sizeof(Int32) == 4,
+                                  bool>::type = true>
+Int32 ctz(Int32 x) {
+  return __builtin_ctz(x);
+}
+
+class FixedBlockSizeMemPool;
+
+/**
+ * @brief Fix-sized page which holds a number of memory blocks.
+ * Blocks in the same page have the same size. A [MemPage] must
+ * be [Size] aligned. A [MemPage] should not constructed directly
+ * because we might want to make many continuous pages.
+ */
+template <size_t Size = 4096>
+class MemPage {
+ public:
+  struct ListNode {
+    ListNode *next_;
+  };
+
+  /**
+   * @brief Metadata of this page. Block size is not
+   * stored in metadata to decrease memory footprint.
+   */
+  struct Meta {
+    FixedBlockSizeMemPool *pool_base_;
+    ListNode *plist_free_;
+  };
+
+  Meta meta_;
+
+  // Data array holds the real block data.
+  char data_[Size - sizeof(Meta)];
+
+  MemPage() = delete;
+  ~MemPage() = delete;
+
+  /**
+   * @brief Initialize all needed fiels in this page.
+   * @param block_size Size of a block in byte. It should be at least
+   * sizeof(ListNode).
+   * @param pool_base Pointer to the whole pool.
+   */
+  inline void init(size_t block_size,
+                   FixedBlockSizeMemPool *pool_base) noexcept {
+    meta_.pool_base_ = pool_base;
+    size_t block_num = (Size - sizeof(Meta)) / block_size;
+
+    // Apending all blocks to the free list.
+    meta_.plist_free_ = reinterpret_cast<ListNode *>(data_);
+    ListNode *cur = meta_.plist_free_;
+    for (size_t i = 1; i < block_num; ++i, cur = cur->next_) {
+      cur->next_ = reinterpret_cast<ListNode *>(&data_[i * block_size]);
+    }
+    cur->next_ = nullptr;
+  }
+
+  /**
+   * @brief Allocate a block from this page.
+   * If there's no available data in page, a nullptr is returned.
+   */
+  inline void *allocate_block() noexcept {
+    ListNode *cur = meta_.plist_free_;
+    if (cur != nullptr) {
+      meta_.plist_free_ = cur->next_;
+    }
+    return cur;
+  }
+
+  /**
+   * @brief Put the ptr back to page.
+   * It's the caller's duty to guarentee the ptr is allocated from this page.
+   */
+  inline void deallocate_block(void *ptr) noexcept {
+#ifndef NDEBUG
+    // Check if the ptr is from this page
+    if (ptr < this || ptr >= this + Size) {
+      fprintf(stderr, "Error: deallocate an external pointer to this page!\n");
+    }
+#endif
+    ListNode *node = reinterpret_cast<ListNode *>(ptr);
+    node->next_ = meta_.plist_free_;
+    meta_.plist_free_ = node;
+  }
+
+  inline void print_free_list() noexcept {
+    fprintf(stdout, "ListNode of %p\n", this);
+    for (ListNode *cur = meta_.plist_free_; cur != nullptr; cur = cur->next_) {
+      fprintf(stdout, "%p\n", cur);
+    }
+  }
+};
+
+/**
+ * @brief A memory pool contains serveral pages with the same page size.
+ * All blocks in the same pool also have the same block size.
+ * Due to alignment issues, do not construct [MemPool] directly.
+ * Instead, use the [create] method.
+ */
+class FixedBlockSizeMemPool {
+ public:
+  static constexpr size_t PageSize = 4096;
+  using Page = MemPage<PageSize>;
+
+  struct Meta {
+    bool owned;
+    size_t block_size_;
+    size_t page_num_;
+    Page *page_base_;
+    Page *page_end_;
+  };
+
+  static_assert(sizeof(Meta) <= PageSize,
+                "Metadata size must not exceed 4K!!!");
+
+  Meta meta_;
+
+  /**
+   * @brief Create a [MemPool] instance from given parameters.
+   * @param block_size Byte size of blocks in page in pool. Block size should be
+   * at least the size of a [ListNode].
+   * @param page_num Number of pages in this pool.
+   * @param pool_base If provided, the create method will not malloc data by
+   * iteself but use the [pool_base] address. It's the caller's response to
+   * guarantee there's enough space pointed by the pointer. This parameter
+   * should be provided with [page_base] at the same time.
+   * @param page_base Similar as previous one, but points to the base address of
+   * first page in pool.
+   */
+  static inline FixedBlockSizeMemPool *create(
+      size_t block_size, size_t page_num, void *pool_base = nullptr,
+      void *page_base = nullptr) noexcept {
+    if (pool_base == nullptr && page_base == nullptr) {
+      // Aligned new is C++17 feature. Now we have to manually translate
+      // addresses.
+      size_t size = (page_num + 1) * PageSize +
+                    sizeof(Meta);  // extra size to ensure alignment
+      size_t self_ptr_val = reinterpret_cast<size_t>(malloc(size));
+      size_t page_base_ptr_val = self_ptr_val;
+      size_t mask = PageSize - 1;
+      if ((page_base_ptr_val & mask) == 0) {
+        page_base_ptr_val += PageSize;
+      } else {
+        page_base_ptr_val &= ~mask;
+        page_base_ptr_val += PageSize;
+        if (page_base_ptr_val - self_ptr_val < sizeof(Meta)) {
+          page_base_ptr_val += PageSize;
+        }
+      }
+      FixedBlockSizeMemPool *self =
+          reinterpret_cast<FixedBlockSizeMemPool *>(self_ptr_val);
+      self->init(block_size, page_base_ptr_val, page_num);
+      self->meta_.owned = true;
+      return self;
+    } else {
+      FixedBlockSizeMemPool *self =
+          reinterpret_cast<FixedBlockSizeMemPool *>(pool_base);
+      self->init(block_size, reinterpret_cast<size_t>(page_base), page_num);
+      self->meta_.owned = false;
+      return self;
+    }
+  }
+
+  inline void *allocate() noexcept {
+    for (size_t i = 0; i < meta_.page_num_; ++i) {
+      void *ptr = meta_.page_base_[i].allocate_block();
+      if (ptr != nullptr) {
+        return ptr;
+      }
+    }
+    // If all pages are full, malloc if by libc.
+    return malloc(meta_.block_size_);
+  }
+
+  /**
+   * @brief Give a pointer back to the pool.
+   * It's the caller's duty to guarantee the ptr is allocated from this pool.
+   */
+  inline void deallocate(void *ptr) noexcept {
+    // If everything is right, it never reaches this branch.
+    // Because the outer level pool will do necessary checks.
+    // But we add this for more safety. This behavior will be tested in CTEST.
+    if (ptr < meta_.page_base_ || ptr >= meta_.page_end_) {
+      free(ptr);
+      return;
+    }
+    Page *page = reinterpret_cast<Page *>(reinterpret_cast<size_t>(ptr) &
+                                          ~(PageSize - 1));
+    page->deallocate_block(ptr);
+  }
+
+  /**
+   * @brief Give a pointer back to the pool.
+   * It's the caller's duty to guarantee the ptr is allocated from this pool.
+   * This version has no check about the pointer.
+   */
+  inline void deallocate_unsafe(void *ptr) noexcept {
+    Page *page = reinterpret_cast<Page *>(reinterpret_cast<size_t>(ptr) &
+                                          ~(PageSize - 1));
+    page->deallocate_block(ptr);
+  }
+
+  FixedBlockSizeMemPool() = delete;
+  ~FixedBlockSizeMemPool() {
+    if (meta_.owned) {
+      free(static_cast<void *>(this));
+    }
+  }
+
+ protected:
+  inline void init(size_t block_size, size_t page_base,
+                   size_t page_num) noexcept {
+    meta_.page_num_ = page_num;
+    meta_.block_size_ = block_size;
+    meta_.page_base_ = reinterpret_cast<Page *>(page_base);
+    meta_.page_end_ =
+        reinterpret_cast<Page *>(page_base + sizeof(Page) * page_num);
+    for (size_t i = 0; i < page_num; ++i) {
+      meta_.page_base_[i].init(block_size, this);
+    }
+  }
+};
+
+/**
+ * @brief A [MemPool] contains serveral different sized [FixedBlockSizeMemPool],
+ * which cache a number of small chunck of memory.
+ * On allocation, MemPool will choose a suitable pool and allocate memory
+ * from it. On deallocation, MemPool can recognize whether the pointer
+ * comes from the MemPool cache or directly from libc malloc and send the
+ * data back to MemPool cache or system memory correctly.
+ */
+class MemPool {
+ public:
+  // Number of different block sizes;
+  static constexpr size_t SizeNum = 8;
+  // An array containing the block_size:num_of_page pairs.
+  // TODO: Add static check to guarantee SizeDist is valid.
+  static constexpr std::pair<size_t, size_t> SizeDist[SizeNum] = {
+      {8, 16},  {16, 16}, {32, 16}, {64, 8},
+      {128, 8}, {256, 4}, {512, 4}, {1024, 4}};
+  static constexpr size_t PageSize = FixedBlockSizeMemPool::PageSize;
+  // Size threshold about whether the MemPool caches it.
+  static constexpr size_t Threshold = SizeDist[SizeNum - 1].first;
+
+  using Page = MemPage<PageSize>;
+
+  struct Meta {
+    void *pool_begin_;
+    void *pool_end_;
+    FixedBlockSizeMemPool *pool[SizeNum];
+  };
+
+  static_assert(sizeof(Meta) <= PageSize, "Metadata must fit in a page.");
+
+  Meta meta_;
+
+  MemPool() = delete;
+  ~MemPool() { free(static_cast<void *>(this)); }
+
+  static MemPool *create() noexcept {
+    // Aligned new is C++17 feature. Now we have to manually translate
+    // addresses.
+    size_t need_page_num = 0;
+    for (size_t i = 0; i < SizeNum; ++i) {
+      need_page_num += SizeDist[i].second + 1;
+    }
+    size_t meta_ptr_val = reinterpret_cast<size_t>(
+        malloc((need_page_num + 1) * PageSize + sizeof(Meta)));
+    size_t pool_ptr_val = meta_ptr_val;
+    if ((meta_ptr_val & (PageSize - 1)) == 0) {
+      // If it's already page aligned.
+      pool_ptr_val += PageSize;
+    } else {
+      pool_ptr_val &= ~(PageSize - 1);
+      pool_ptr_val += PageSize;
+      if (pool_ptr_val - meta_ptr_val < sizeof(Meta)) {
+        pool_ptr_val += PageSize;
+      }
+    }
+    MemPool *self = reinterpret_cast<MemPool *>(meta_ptr_val);
+    self->meta_.pool_begin_ = reinterpret_cast<void *>(pool_ptr_val);
+    self->meta_.pool_end_ =
+        reinterpret_cast<void *>(pool_ptr_val + need_page_num * PageSize);
+    for (size_t cur = pool_ptr_val, i = 0; i < SizeNum;
+         cur += (SizeDist[i].second + 1) * PageSize, i += 1) {
+      size_t block_size = SizeDist[i].first;
+      size_t page_num = SizeDist[i].second;
+      void *pool_base = reinterpret_cast<void *>(cur);
+      void *page_base = reinterpret_cast<void *>(cur + PageSize);
+      self->meta_.pool[i] = FixedBlockSizeMemPool::create(block_size, page_num,
+                                                          pool_base, page_base);
+    }
+    return self;
+  }
+
+  inline size_t get_pool_id(size_t round_size) const noexcept {
+    // A hack here because the smallest round_size is 8, which is 2**3;
+    return ctz(round_size) - 3;
+  }
+
+  void *allocate(size_t size) noexcept {
+    if (size > Threshold) {
+      return malloc(size);
+    }
+    size_t round_size = std::max(round2pow(size), SizeDist[0].first);
+    size_t id = get_pool_id(round_size);
+    return meta_.pool[id]->allocate();
+  }
+
+  void deallocate(void *ptr) noexcept {
+    if (ptr < meta_.pool_begin_ || ptr >= meta_.pool_end_) {
+      free(ptr);
+      return;
+    }
+    Page *page = reinterpret_cast<Page *>(reinterpret_cast<size_t>(ptr) &
+                                          ~(PageSize - 1));
+    page->deallocate_block(ptr);
+  }
+};
+
+// In C++11, we have to redeclare them in namespace scpoe again.
+constexpr size_t MemPool::SizeNum;
+constexpr std::pair<size_t, size_t> MemPool::SizeDist[MemPool::SizeNum];
+constexpr size_t MemPool::PageSize;
+constexpr size_t MemPool::Threshold;
+
+}  // namespace Detail
+}  // namespace UAllocator
+
+#endif
--- tests/test_mem_pool.cpp
+++ tests/test_mem_pool.cpp
@@ -4,20 +4,20 @@
 #include <random>
 
 #include "../src/allocator.h"
+#include "../src/mem_pool.h"
 
 using namespace UAllocator::Detail;
 
 int test_fixed_size_pool_single_size(size_t block_size, size_t page_num,
-                                     size_t repeate = size_t(1e5),
+                                     size_t batch_num = size_t(1e2),
                                      size_t batch_size = size_t(1e3)) {
   using Page = FixedBlockSizeMemPool::Page;
   FixedBlockSizeMemPool *pool =
       FixedBlockSizeMemPool::create(block_size, page_num);
   int prevent_opt = ~0;
-  size_t batch;
-  for (size_t rd = 0; rd < repeate;) {
+  for (size_t rd = 0; rd < batch_num; ++rd) {
     std::vector<void *> allocated;
-    for (batch = 0; batch < batch_size && rd + batch < repeate; ++batch, ++rd) {
+    for (size_t id = 0; id < batch_size; ++id) {
       char *ptr = (char *)pool->allocate();
       if (ptr == nullptr) {
         fprintf(stderr, "Allocation failed.\n");
@@ -34,17 +34,17 @@ int test_fixed_size_pool_single_size(size_t block_size, size_t page_num,
       }
       allocated.push_back((void *)ptr);
     }
-    for (size_t bt = 0; bt < batch; ++bt) {
+    for (size_t id = 0; id < batch_size; ++id) {
       char *ptr = (char *)allocated.back();
       allocated.pop_back();
       for (size_t b = 0; b < block_size; ++b) {
         if (ptr[b] != 'a' + (b % 26)) {
-          fprintf(stderr,
-                  "Block size: %lu, page num: %lu, round %lu, byte %lu, "
-                  "expected %c but "
-                  "get %c.\n",
-                  block_size, page_num, rd - 1 - bt, b, char('a' + (b % 26)),
-                  ptr[b]);
+          fprintf(
+              stderr,
+              "Block size: %lu, page num: %lu, round %lu, batch %lu, byte %lu, "
+              "expected %c but "
+              "get %c.\n",
+              block_size, page_num, rd, id, b, char('a' + (b % 26)), ptr[b]);
           return -1;
         }
         prevent_opt &= ptr[b] - char('a' + (b % 26));
@@ -67,4 +67,39 @@ int test_fixed_size_pool() {
   return result;
 }
 
-int main() { return 0 || test_fixed_size_pool(); }
+int test_mem_pool(size_t batch_num = size_t(1e2),
+                  size_t batch_size = size_t(1e3)) {
+  std::random_device rd{};
+  std::mt19937 gen{rd()};
+  std::uniform_int_distribution<> dist(1, 3000);
+  auto pool = MemPool::create();
+  int prevent_opt = ~0;
+  for (size_t cur_b = 0; cur_b < batch_num; ++cur_b) {
+    std::vector<std::pair<char *, size_t>> allocated;
+    for (size_t id = 0; id < batch_size; ++id) {
+      size_t size = dist(gen);
+      char *ptr = (char *)pool->allocate(size);
+      for (size_t b = 0; b < size; ++b) {
+        ptr[b] = 'a' + char(b % 26);
+      }
+      allocated.push_back({ptr, size});
+    }
+    for (size_t id = 0; id < batch_size; ++id) {
+      size_t size = allocated.back().second;
+      char *ptr = allocated.back().first;
+      allocated.pop_back();
+      for (size_t b = 0; b < size; ++b) {
+        if (ptr[b] != 'a' + char(b % 26)) {
+          fprintf(stderr,
+                  "Round %lu, batch %lu, byte %lu, expected %c but get %c\n",
+                  cur_b, id, b, char('a' + char(b % 26)), ptr[b]);
+          return -1;
+        }
+        prevent_opt &= ptr[b] - char('a' + char(b % 26));
+      }
+    }
+  }
+  return prevent_opt;  // is always 0 if correct.
+}
+
+int main() { return 0 || test_fixed_size_pool() || test_mem_pool(); }
